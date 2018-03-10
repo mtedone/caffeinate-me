41962ed7d093fd37f991b7e32a0bd40f1f5ecc04
Marco Tedone <marco.tedone@gmail.com>
2018-03-10 09:35:59 +0000

    Improved on lesson 6

--- src/test/java/caffeinateme/steps/CoffeeOrdersClient.java
+++ src/test/java/caffeinateme/steps/CoffeeOrdersClient.java
@@ -2,6 +2,7 @@ package caffeinateme.steps;
 
 import caffeinateme.Order;
 import caffeinateme.OrderReceipt;
+import net.thucydides.core.annotations.Step;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,16 +11,19 @@ public class CoffeeOrdersClient {
 
     List<Order> orders = new ArrayList<>();
 
+    @Step("Place order for customer {0} for {1} x {2}")
     public OrderReceipt placeOrder(long customerId, int quantity, String product) {
         Order order = new Order(customerId, quantity, product);
         orders.add(order);
         return order.getReceipt();
     }
 
+    @Step
     public List<Order> getOrders() {
         return new ArrayList<>(orders);
     }
 
+    @Step("Notifies updated ETA for customer {0} who is {1} minutes away from the shop")
     public void updateCustomerEta(long customerId, int minutesAway) {
         orders.stream().filter(order -> order.getClientId() == customerId)
                 .forEach(order -> order.updateEtaTo(minutesAway));
